68cec66b16fe03e9d115e3a887c31a8d8ca6fb2d
Maria Eduarda da Silva <88351641+httpmariaed@users.noreply.github.com>
2024-09-18 07:13:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+
+    {
+    titulo : 'Rayssa Andrade',
+    descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags : "skate fada sk8"
+    },
+     {
+        titulo: 'Rebeca Andrade',
+        descricao: "Rebeca Andrade é uma ginasta brasileira que se destacou nos Jogos Olímpicos de Tóquio 2020, conquistando medalhas históricas para o Brasil. Sua força e determinação a tornaram um ícone do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ouro ginasta ginastica "
+      },
+      {
+        titulo: 'Beatriz Souza',
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou diversos títulos nacionais e internacionais. Sua técnica e força a colocaram entre as principais atletas do judô mundial.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza#:~:text=Beatriz%20Souza%20(Itariri%2C%2020%20de,ex%C3%A9rcito%20e%20campe%C3%A3%20ol%C3%ADmpica%20brasileira.&text=Beatriz%20conquistou%20a%20primeira%20medalha,hist%C3%B3ria%20do%20Esporte%20Clube%20Pinheiros.",
+        tags: "judô judoca judo ouro"
+      }
+];
